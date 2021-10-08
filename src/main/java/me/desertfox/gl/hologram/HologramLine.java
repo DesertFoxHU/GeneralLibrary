748b0a8bc6af702f71504a05e7237edff6dabb38
DesertFoxHU <40893862+DesertFoxHU@users.noreply.github.com>
2021-10-08 19:16:53 +0200

    Added physical holograms, range
    [+] Added Hologram class similar to HolographicDisplays, which make holograms possible via server-sided entities.
    [+] Added Range which can store a min and max double variables

---
+++ src/main/java/me/desertfox/gl/hologram/HologramLine.java
@@ -0,0 +1,52 @@
+package me.desertfox.gl.hologram;
+
+import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+
+public class HologramLine {
+
+    private int index;
+    private ArmorStand armorStand;
+    private String line;
+
+    public HologramLine(Location location, int index, String line) {
+        this.index = index;
+        this.line = line;
+        this.armorStand = (ArmorStand) location.getWorld().spawnEntity(location, EntityType.ARMOR_STAND);
+        armorStand.setVisible(false);
+        armorStand.setGravity(false);
+        armorStand.setCollidable(false);
+        armorStand.setCustomName(line);
+        armorStand.setCustomNameVisible(true);
+    }
+
+    public void remove(){
+        armorStand.remove();
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public ArmorStand getArmorStand() {
+        return armorStand;
+    }
+
+    public void setArmorStand(ArmorStand armorStand) {
+        this.armorStand = armorStand;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+        armorStand.setCustomName(line);
+    }
+}
---
+++ src/main/java/me/desertfox/gl/region/LocationStorage.java
@@ -0,0 +1,30 @@
+package me.desertfox.gl.region;
+
+import me.desertfox.gl.region.Cuboid;
+import org.bukkit.Location;
+
+public class LocationStorage {
+
+    private Location loc1;
+    private Location loc2;
+
+    public Cuboid getAsCuboid(){
+        return new Cuboid(loc1, loc2);
+    }
+
+    public Location getLoc1() {
+        return loc1;
+    }
+
+    public void setLoc1(Location loc1) {
+        this.loc1 = loc1;
+    }
+
+    public Location getLoc2() {
+        return loc2;
+    }
+
+    public void setLoc2(Location loc2) {
+        this.loc2 = loc2;
+    }
+}
